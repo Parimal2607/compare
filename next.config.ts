765a511cd7ff1c98f99891d9f95ee6f5222776e6
Parimal2607 <parimal81280@gmail.com>
2026-07-10 12:33:11 +0530

    Allow all HTTPS image hostnames via remotePatterns catch-all

--- next.config.ts
+++ next.config.ts
@@ -5,23 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "fdn2.gsmarena.com",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-      {
-        protocol: "https",
-        hostname: "play-lh.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "akm-img-a-in.tosshub.com",
+        hostname: "**",
       },
     ],
   },
